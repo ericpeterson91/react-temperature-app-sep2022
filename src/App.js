dbc48873cb585bbeb4899d22c0881c0ba546549e
Eric Peterson <ericpeterson91@live.ca>
2022-09-02 10:14:02 -0600

    temperature counts up and down, and changes color at 15 degrees

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [temp, setTemp] = useState(12)
+  const [tempColor, setTempColor] = useState('cold')
+
+  const increaseTemp = () => {
+    if(temp >= 30) return
+    
+    if (temp >= 14) {
+      setTempColor('hot')
+    }
+    setTemp(temp + 1)
+    
+  }
+
+  const decreaseTemp = () => {
+    if(temp <= 0) return
+    if (temp <= 15) {
+      setTempColor('cold')
+    }
+    setTemp(temp - 1)
+   
+  }
+
+
+
   return (
     <div className='app-container'>
       <div className="temperature-display-container">
-        <div className="temperature-display">10°C</div>
+        <div className={`temperature-display ${tempColor}`}>{temp}°C</div>
       </div>
       <div className="button-container">
-        <button>+</button>
-        <button>-</button>
+        <button onClick={increaseTemp}>+</button>
+        <button onClick={decreaseTemp}>-</button>
       </div>
     </div>
   )
